f5a8c58f84fd04fc16bf022c73569f23d3e2ef95
ellisa <ellisaissa@gmail.com>
2021-07-03 21:21:47 -0400

    env file
    created env file added to routes

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -7,12 +7,49 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  router.get('/', (req, res) => {
+    Product.findAll({
+      include: [{
+              model: Category,
+              attributes: ['category_name']
+          },
+          {
+              model: Tag,
+              attributes: ['tag_name']
+          }
+      ]
+  })
+  .then(dbProductData => res.json(dbProductData))
+  .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+  });
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  router.get('/:id', (req, res) => {
+    Product.findOne({
+      where: {
+        id: req.params.id,
+      },
+      attributes: ['id', 'product_name', 'price', 'stock'],
+      include: [
+        Category,
+        attributes, ["category_name"],
+        {
+          model: Tag,
+          through: ProductTag,
+        },
+      ],
+    })
+      .then((products) => res.json(products))
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
 });
 
 // create new product
@@ -91,6 +128,23 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+//   try {
+//     const productData = await Product.destroy({
+//       where: {
+//         id: req.params.id,
+//       },
+//     });
+
+//     if (!productData) {
+//       res.status(404).json({ message: "No product found with this id!" });
+//       return;
+//     }
+
+//     res.status(200).json(productData);
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+});
 });
 
-module.exports = router;
+module.exports = router})
